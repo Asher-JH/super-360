a6fc3cd3e67a7fe0f7b85579e41a4d7b200fb6e8
Asher Chan <asherchan2002@hotmail.com>
2022-04-13 22:40:57 +0800

    fix errors on production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ["./src/views/pages/*.{html,ejs}", "./src/views/partials/*.{html,ejs}"],
+  content: [
+    './src/views/pages/*.{html,ejs}',
+    './src/views/partials/*.{html,ejs}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-orange': '#FF8008',
+        'secondary-orange': '#FFC837',
+        'light-grey': '#BCBCBC',
+      },
+    },
   },
   plugins: [],
-}
+};
