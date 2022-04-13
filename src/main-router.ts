d5259222153d067affd68e6d11bea01a0acfa497
Asher Chan <asherchan2002@hotmail.com>
2022-04-14 01:29:27 +0800

    add pages express routes

--- src/main-router.ts
+++ src/main-router.ts
@@ -6,4 +6,20 @@ router.get('/', (_req: Request, res: Response) => {
   res.render('index', {});
 });
 
+router.get('/about-us', (_req: Request, res: Response) => {
+  res.render('index', {});
+});
+
+router.get('/services', (_req: Request, res: Response) => {
+  res.render('index', {});
+});
+
+router.get('/packages', (_req: Request, res: Response) => {
+  res.render('index', {});
+});
+
+router.get('/contact', (_req: Request, res: Response) => {
+  res.render('index', {});
+});
+
 export default router;
