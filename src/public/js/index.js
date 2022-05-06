d48cd38f345e7a2e8d73c9eeed4d1fd8987230a3
Asher Chan <asherchan2002@hotmail.com>
2022-05-07 03:43:49 +0800

    main scroll virtual tour

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,4 +1,8 @@
 const btns = document.querySelectorAll('.copy-link-btn');
+const jumbo = document.getElementById('jumbo');
+const jumboContainer = document.getElementById('jumbo-container');
+const mainTour = document.getElementById('main-tour');
+const header = document.getElementsByTagName('header');
 
 btns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
@@ -11,4 +15,36 @@ btns.forEach((btn) => {
 
     navigator.clipboard.writeText(url.value);
   });
+});
+
+jumbo.addEventListener('wheel', (e) => {
+  if (e.deltaY > 0) {
+    e.preventDefault();
+    e.stopPropagation();
+    jumboContainer.style.opacity = 0;
+    header[0].style.opacity = 0;
+
+    setTimeout(() => {
+      jumboContainer.style.display = 'none';
+      header[0].style.display = 'none';
+      mainTour.style.opacity = 1;
+    }, 300);
+  }
+});
+
+document.addEventListener('scroll', () => {
+  if (
+    (window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop) === 0
+  ) {
+    jumboContainer.style.opacity = 1;
+    header[0].style.opacity = 1;
+
+    setTimeout(() => {
+      jumboContainer.style.display = 'block';
+      header[0].style.display = 'block';
+      mainTour.style.opacity = 0;
+    }, 300);
+  }
 });
